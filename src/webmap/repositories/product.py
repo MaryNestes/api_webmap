9917c5292cf373aa703c12a19720477bdd6f9714
Mary Nesterenko <masha.nesterenko.1999@gmail.com>
2022-10-11 17:21:39 +0700

    was fixed error with value retrieval

--- src/webmap/repositories/product.py
+++ src/webmap/repositories/product.py
@@ -6,7 +6,6 @@ from ..models.product import TypeProductOut, CommonProdItemsIn, CommonTimeItemsI
 class ProductRepository(BaseRepository):
 
     async def get_all_data(self):
-        # query = DateCoord.select().with_only_columns([DateCoord.c.dt_time])
         query = "SELECT DISTINCT dt_time::date from date_coord;"
         return await self.database.fetch_all(query=query)
 
@@ -15,11 +14,12 @@ class ProductRepository(BaseRepository):
             query=f"SELECT dt_time::time, coord from date_coord where dt_time::date = date '{date}'; ")
 
     async def get_value_by_lon_lat(self, data: CommonProdItemsIn):
+        # return {'lon': data.lon, 'lat': data.lat}
         return await self.database.fetch_all(
-            query=f'''select lvl_{data.level}[get_index_by_lonlat({data.lon}, {data.lat},'{data.date} {data.time}')] as lvl_value
+            query=f'''select lvl_{data.level}[check_index({data.lon}, {data.lat},'{data.date} {data.time}')] as lvl_value
                 from products inner join type_prod on type_prod.id=products.type_id
-    			  inner join value_lvls on value_lvls.id=products.value_id
-                where  name = '{data.tp}' and lvl_{data.level} is not null and products.dt_time = '{data.date} {data.time}';''')
+    		  inner join value_lvls on value_lvls.id=products.value_id
+            where  name = '{data.tp}' and lvl_{data.level} is not null and products.dt_time = '{data.date} {data.time}';''')
 
     async def get_type(self, data: CommonTimeItemsIn):
         return await self.database.fetch_all(query=f'''
